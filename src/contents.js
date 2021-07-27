0ae3c01a6b9d83e40f1ecc51364dad887aa9d4fd
sakthi <sakthivel.m2418@gmai.com>
2021-07-27 18:33:08 +0530

    final commit

--- src/contents.js
+++ src/contents.js
@@ -3,29 +3,22 @@ import './hint.css';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css'
 import { Fade } from "react-awesome-reveal";
-import Typewriter from 'typewriter-effect'
 import React, { Component, } from 'react';
 
-
 class Hint extends Component {
-
-
   hints = [{ id: 1, hint: 'Its not a Rocket Science!' },
   { id: 2, hint: 'Multiply the current Number by two!' },
   { id: 3, hint: 'So tough? just add the given number' },
   { id: 4, hint: 'Divide the number by 2' },
   { id: 5, hint: 'Subtract the number you thought!' }]
   render() {
-    return (
-      
-      
+    return (  
          this.hints.filter(thint => thint.id === this.props.ir).map(thint => (
         console.log(thint),<div className='hint' key={thint.id}>
       <Tippy  className="tool" content={thint.hint}>
       <span className="buthint">hint??</span>
       </Tippy>
-      </div>
-      
+      </div>     
      )))
   }
 }
@@ -33,7 +26,7 @@ class Idea extends Component {
  
   ideas = [{ id: 1, idea: 'Think a Number' },
   { id: 2, idea: 'I am Giving same number you thought!' },
-  { id: 3, idea: 'Now Your Favourite person is giving You ',},
+  { id: 3, idea: 'Now Your Favourite person is giving You ',ied:" take it!"},
   { id: 4, idea: 'Donate  half the value to government ' },
   { id: 5, idea: 'Now Give back what I gave' },
   { id: 6, idea: ' ',},
@@ -46,7 +39,7 @@ class Idea extends Component {
       this.ideas.filter(ide => ide.id === this.props.ir).map(ide => (
         <div  key={ide.id}><div className="pos">
          <Fade delay={100}>
-         <span> {ide.idea}
+         <span> {ide.idea} {ide.ied}
         {this.props.ir==3 && <span>{this.props.numb}</span>}</span>
         </Fade>
         {console.log(this.props.numb)}
